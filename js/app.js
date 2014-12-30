3f693fc277c98188505d624351db375e7b57b3e9
jongrover <hello@jonathangrover.com>
2014-12-29 20:43:57 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+var dogs = new Dogs(); // Create a new Dogs Collection.
+var router = new Router(); // Create a new Router to handle routes.
+Backbone.history.start(); // Strat tracking routes history.
+router.navigate('dogs'); // Start our app at the #dogs route by default.
---
+++ js/models/dog.js
@@ -0,0 +1,8 @@
+Dog = Backbone.Model.extend({
+
+  defaults: {
+    fullname: 'unknown',
+    age: 0,
+    fleas: false
+  }
+});
